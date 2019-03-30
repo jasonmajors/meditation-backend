93524b1b3e4df71bacade3a544cd12b2b9731cc7
Jason Majors <jasonrmajors@gmail.com>
2019-03-29 17:56:31 -0700

    Fixed bug with generated dir in the wrong place

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { APP_SECRET, getUserId } = require('../utils')
 
-async function signup(parent, args, context, info) {
+async function signup(parent, args, context) {
   const password = await bcrypt.hash(args.password, 10)
   const user = await context.prisma.createUser({...args, password})
   const token = jwt.sign({ userId: user.id }, APP_SECRET)
@@ -13,6 +13,21 @@ async function signup(parent, args, context, info) {
   }
 }
 
+async function meditation(parent, args, context) {
+  // Just abusing this to ensure the user is authenticated right now
+  // Will at some point add a created_by field to the Meditation records
+  getUserId(context);
+
+  const meditation = await context.prisma.createMeditation({
+    title: args.title,
+    description: args.description,
+    url: args.url,
+  })
+
+  return meditation
+}
+
 module.exports = {
   signup,
+  meditation,
 }
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,9 +1,22 @@
+/**
+ * Fetches the Users through the prisma API
+ */
 async function users(parent, args, context) {
-  const users = await context.prisma.users();
+  const users = await context.prisma.users()
 
-  return users;
+  return users
+}
+
+/**
+ * Fetches the Meditations through the prisma API
+ */
+async function meditations(parent, args, context) {
+  const meditations = await context.prisma.meditations()
+
+  return meditations
 }
 
 module.exports = {
   users,
+  meditations,
 }
