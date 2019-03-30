800646ab0af452b1759e79f08932a032120ae728
Jason Majors <jasonrmajors@gmail.com>
2019-03-29 21:17:19 -0700

    Add login functionality to request a token

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,6 +2,9 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { APP_SECRET, getUserId } = require('../utils')
 
+/**
+ * The resolver for the signup mutation
+ */
 async function signup(parent, args, context) {
   const password = await bcrypt.hash(args.password, 10)
   const user = await context.prisma.createUser({...args, password})
@@ -13,6 +16,32 @@ async function signup(parent, args, context) {
   }
 }
 
+/**
+ * The resolver for the login mutation
+ */
+async function login(parent, args, context) {
+  const AUTH_ERROR = `Invalid credentials`
+  const user = await context.prisma.user({ email: args.email });
+  if (!user) {
+    throw new Error(AUTH_ERROR);
+  }
+
+  const valid = await bcrypt.compare(args.password, user.password)
+  if (!valid) {
+    throw new Error(AUTH_ERROR)
+  }
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+  return {
+    token,
+    user,
+  }
+}
+
+/**
+ * The resolver for the meditation mutation
+ */
 async function meditation(parent, args, context) {
   // Just abusing this to ensure the user is authenticated right now
   // Will at some point add a created_by field to the Meditation records
@@ -29,5 +58,6 @@ async function meditation(parent, args, context) {
 
 module.exports = {
   signup,
+  login,
   meditation,
 }
