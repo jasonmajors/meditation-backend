faf6e51477f32e592da6bbe1f4657242b465fbc2
Jason Majors <jasonrmajors@gmail.com>
2019-06-19 20:46:30 -0700

    Able to read signed cookie

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const jwt = require('jsonwebtoken');
-const { verifyJwt, getKey } = require('./utils')
+const jwks = require('jwks-rsa');
 
 dotenv.config();
 // TODO: This whole file could use some TS types
@@ -28,21 +28,52 @@ const server = new GraphQLServer({
 })
 
 // TODO: Move this express middleware shit into a module?
+// TODO: Move strings to env and typescript types would be nice
+function getKey(header, callback) {
+  const client = jwks({
+    jwksUri: 'https://knurling.auth0.com/.well-known/jwks.json'
+  });
+
+  client.getSigningKey(header.kid, function(err, key) {
+    var signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+}
+
+// TODO: Add support to issue our own JWT rather than a cookie to give the option
+// for nonbrowser clients to simply use JWTs
+function verifyJwt(token, response) {
+  jwt.verify(
+    token,
+    getKey,
+    { audience: 'https://knurling.api.com', issuer: 'https://knurling.auth0.com/' },
+    function (err, decoded) {
+      if (decoded) {
+        issueCookie(decoded, response)
+      } else {
+        authErrorResponse(response, err)
+      }
+    }
+  )
+}
+
 function authErrorResponse(response, err) {
   response.status(401).json({ 'error': err })
 }
 
-function issueCookie(token, response) {
-  response.cookie('token', token, { httpOnly: true, secure: process.env.APP_ENV === 'production' })
-    .sendStatus(200)
+function issueCookie(decoded, response) {
+  response.cookie(
+    'knurling.auth',
+    decoded,
+    { httpOnly: true, secure: process.env.APP_ENV === 'production', signed: true }
+  ).sendStatus(200)
 }
-
-server.express.options(process.env.CLIENT_URL, cors())
-server.express.use(cors({
+const corsOptions = {
   origin: process.env.CLIENT_URL,
   credentials: true,
-}))
-server.express.use(cookieParser())
+}
+server.express.use(cookieParser(process.env.APP_SECRET))
+server.express.use(cors(corsOptions))
 
 server.express.post('/authenticate', (req, res) => {
   const authorization = req.get('Authorization')
@@ -51,7 +82,7 @@ server.express.post('/authenticate', (req, res) => {
     if (token) {
       // Set an HTTP cookie if we have a legit access token (jwt) that the browser can use
       // to access the GraphQL API
-      verifyJwt(token, issueCookie, authErrorResponse, res)
+      verifyJwt(token, res)
     } else {
       res.status(401).json({ 'error': 'No auth token provided' })
     }
@@ -60,5 +91,5 @@ server.express.post('/authenticate', (req, res) => {
   }
 })
 
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+server.start({cors: false}, () => console.log(`Server is running on http://localhost:4000`))
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,47 +1,15 @@
 import { Context } from './interfaces/Context'
-// const jwt = require('jsonwebtoken')
-// var jwt = require('express-jwt');
-const jwks = require('jwks-rsa');
-const jwt = require('jsonwebtoken');
+
 const dotenv = require('dotenv');
 const APP_SECRET = process.env.APP_SECRET
 
 dotenv.config();
-// TODO: Move strings to env and typescript types would be nice
-function getKey(header, callback) {
-  const client = jwks({
-    jwksUri: 'https://knurling.auth0.com/.well-known/jwks.json'
-  });
-
-  client.getSigningKey(header.kid, function(err, key) {
-    var signingKey = key.publicKey || key.rsaPublicKey;
-    callback(null, signingKey);
-  });
-}
-
-function verifyJwt(token, onSuccess, onFailure, response) {
-  jwt.verify(
-    token,
-    getKey,
-    { audience: 'https://knurling.api.com', issuer: 'https://knurling.auth0.com/' },
-    function (err, decoded) {
-      if (decoded) {
-        console.log(decoded)
-        onSuccess(token, response)
-      } else {
-        onFailure(response, err)
-      }
-    }
-  )
-}
 
 function hasPermission(context: Context, permission: string): boolean {
-  // TODO: Read the fucking cookie
-  // If we make the cookie hold the JWT, can use the old jwt.verify with a function to validate the signature
-  // Should do the above one
-  // Or can just use the current jwtCheck maybe without the rateLimit
+  console.log('signed cookies: ', context.request.signedCookies)
   return false;
 }
+
 function authorizedUser(context: Context) {
   // const Authorization = context.request.get('Authorization')
   // if (Authorization) {
@@ -57,6 +25,4 @@ module.exports = {
   APP_SECRET,
   authorizedUser,
   hasPermission,
-  verifyJwt,
-  getKey
 }
